85e93102d89863d45dcbe90c880454b78f1cbb3c
htomcat <gllu--u.com@hotmail.co.jp>
2018-08-11 15:47:17 +0900

    [add] cross page test

---
+++ qa/tests-crosspage.js
@@ -0,0 +1,38 @@
+var Browser = require('zombie'), assert = require('chai').assert;
+
+var browser;
+
+suite('Cross-Page Tests', function(){
+    setup(function(){
+        browser = new Browser();
+    });
+
+    test('requesting', function(done){
+        var referrer = 'http://localhost:3000/tours/hood-river';
+        broswer.visit(referrer, function(){
+            browser.clickLink('.requestGroupRate', function(){
+                assert(browser.field('referrer').value === referrer;
+                done();
+            });
+        });
+    });
+
+    test('requesting', function(done){
+        var referrer = 'http://localhost:3000/tours/oregon-coast';
+        broswer.visit(referrer, function(){
+            browser.clickLink('.requestGroupRate', function(){
+                assert(browser.field('referrer').value === referrer;
+                done();
+            });
+        });
+    });
+
+    test('visting', function(done){
+        broswer.visit(referrer, function(){
+            browser.visit('http://localhost:3000/tours/request-group-rate', function(){
+                assert(browser.field('referrer').value === '';
+                done();
+            });
+        });
+    });
+}
