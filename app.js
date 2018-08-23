e57b54a59e3846d5669974b3978324a7630a7147
htomcat <gllu--u.com@hotmail.co.jp>
2018-08-23 09:50:35 +0900

    [modify] implement root to newsletter

--- app.js
+++ app.js
@@ -56,6 +56,20 @@ app.get('/data/nursery-rhyme', function(req, res){
         });
 });
 
+app.get('/newsletter', function(req, res){
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+
+app.post('/process', function(req, res){
+    console.log('Form (from querystring): ' + req.query.form);
+    console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+    console.log('Name (from visible form field): ' + req.body.name);
+    console.log('Email (from visible form field): ' + req.body.email);
+    res.redirect(303, '/thank-you');
+});
+
 // resource
 app.use(express.static(__dirname + '/public'));
 // custom 404 page
