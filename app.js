cbdfbb5eea01fa382360aa27c55bf861c874767a
htomcat <gllu--u.com@hotmail.co.jp>
2018-08-31 10:29:33 +0900

    [modify] use express-session

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.use(require('body-parser').urlencoded({ extended: true }));
 
 // setting and accessing cookies.
 app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret,
+}));
 
 app.get('/', function(req, res){
 	res.render('home');
