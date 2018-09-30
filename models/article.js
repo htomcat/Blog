12010cafe17d3a3c1d84189317d206b061bb9326
htomcat <gllu--u.com@hotmail.co.jp>
2018-09-30 13:46:41 +0900

    [add] article model

---
+++ models/article.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var articleSchema = mongoose.Schema({
+    id: Number,
+    title: String,
+    content: String,
+    images: String,
+});
+articleSchema.methods.getDisplayPrice = function(){
+    //return '$' + (this.priceInCents / 100).toFixed(2);
+    return ''
+};
+var article = mongoose.model('Article', articleSchema);
+module.exports = article;
